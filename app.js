22872b215e75a0d0f8cf36f90693dc864ee80e89
MOHAMMED BEN HARRI <benharri0mohammed@gmail.com>
2020-04-18 14:24:57 +0100

    add main app js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express ()
+const bodyParser = require ('body-parser')
+const booksRoutes = require('./api/routes/book');
+
+/*app.use(bodyParser.urlencoded({
+    extended : false
+}))
+app.use(bodyParser.json())*/
+
+
+
+
+app.use ('/books',booksRoutes);
+
+module.exports  = app;
